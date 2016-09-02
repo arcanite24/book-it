9e8aa8bc8e77df3073abcdbbbb2dd1ae794fa376
root <hazielfe@gmail.com>
2016-09-02 18:49:37 -0500

    Hola

--- assets/js/!app.js
+++ assets/js/!app.js
@@ -18,11 +18,11 @@ app.config(function ($urlRouterProvider, $stateProvider, $mdThemingProvider, $md
     .state('index', {url: '/index', templateUrl: 'templates/index.html', controller: 'MainCtrl'})
     .state('register', {url: '/register', templateUrl: 'templates/register.html', controller: 'MainCtrl'})
     .state('dashboard', {url: '/dashboard', templateUrl: 'templates/dashboard.html', controller: 'DashboardCtrl'})
-    
+
     //Gestion de usuarios
     .state('manage-users', {url: '/users/admin', templateUrl: 'templates/users/manage.html', controller: 'UsersCtrl'})
     .state('add-users', {url: '/users/new', templateUrl: 'templates/users/new.html', controller: 'UsersCtrl'})
-    
-    //Test routes    
+
+    //Test routes
     .state('text-editor-test', {url: '/text-editor/admin', templateUrl: 'templates/text-editor/test.html', controller: 'AdminCtrl'})
 });
